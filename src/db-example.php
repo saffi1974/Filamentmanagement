60d9d89cf7bb683da2f04d9b577c7efc01bf4719
saffi1974 <misaf@gmx.de>
2025-10-03 20:14:53 +0200

    update

--- src/db-example.php
+++ src/db-example.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 $servername = "localhost";
 $username   = "root";
@@ -10,16 +9,3 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 ?>
-=======
-<?php
-$servername = "localhost";
-$username   = "root";
-$password   = "deinpasswort";
-$database   = "filamentlager";
-
-$conn = new mysqli($servername, $username, $password, $database);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-?>
->>>>>>> 3c22cdc (Initial commit)
